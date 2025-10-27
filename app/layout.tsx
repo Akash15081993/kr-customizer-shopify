743023aafcce1ba5f577d3194b9734321cc5982b
Shahil <shahil@plumtreegroup.net>
2025-10-28 01:11:01 +0530

    Embedded app checks V6

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,55 @@
-"use client";
-
+// app/layout.tsx
 import "./globals.css";
 import { ShopProvider } from "./contexts/ShopContext";
 import NextProgressBar from "./components/nextProgress";
-import "@shopify/polaris/build/esm/styles.css";
-import { AppProvider } from "@shopify/polaris";
-import enTranslations from "@shopify/polaris/locales/en.json";
-import AppBridgeProvider from "./providers/AppBridgeProvider";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <NextProgressBar />
-        <AppProvider i18n={enTranslations}>
-          <AppBridgeProvider>
-            <ShopProvider>{children}</ShopProvider>
-          </AppBridgeProvider>
-        </AppProvider>
+        <ShopProvider>
+          <div className="container">{children}</div>
+        </ShopProvider>
       </body>
     </html>
   );
 }
+
+
+// "use client";
+
+// import "./globals.css";
+// import { ShopProvider } from "./contexts/ShopContext";
+// import NextProgressBar from "./components/nextProgress";
+// import "@shopify/polaris/build/esm/styles.css";
+// import { AppProvider } from "@shopify/polaris";
+// import enTranslations from "@shopify/polaris/locales/en.json";
+// import AppBridgeProvider from "./providers/AppBridgeProvider";
+
+// export default function RootLayout({ children }: { children: React.ReactNode }) {
+//   return (
+//     <html lang="en">
+//       <body>
+//         <NextProgressBar />
+//         <AppProvider i18n={enTranslations}>
+//           <AppBridgeProvider>
+//             <ShopProvider>{children}</ShopProvider>
+//           </AppBridgeProvider>
+//         </AppProvider>
+//       </body>
+//     </html>
+//   );
+// }
