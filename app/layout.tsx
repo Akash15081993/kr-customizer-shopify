ce8ece31446c9395c069e889cded6df6d4bda47a
Shahil <shahil@plumtreegroup.net>
2025-10-30 14:55:57 +0530

    U2

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,32 +2,39 @@
 
 import "./globals.css";
 import { Suspense } from "react";
+import { useSearchParams } from "next/navigation";
 import { AppProvider } from "@shopify/polaris";
 import "@shopify/polaris/build/esm/styles.css";
 import enTranslations from "@shopify/polaris/locales/en.json";
 import NextProgressBar from "./components/nextProgress";
 import { ShopProvider } from "./contexts/ShopContext";
 import AppBridgeProvider from "./providers/AppBridgeProvider";
-import { useSearchParams } from "next/navigation";
 
 function InnerProviders({ children }: { children: React.ReactNode }) {
   const searchParams = useSearchParams();
   const host = searchParams.get("host");
   const shop = searchParams.get("shop");
 
+  //If not inside Shopify (no shop/host), skip AppBridgeProvider
   if (!host || !shop) {
-    return <div>Loading Shopify context...</div>;
+    return (
+      <ShopProvider>
+        <div className="container">{children}</div>
+      </ShopProvider>
+    );
   }
 
+  //Inside Shopify → wrap with AppBridgeProvider
   return (
     <AppBridgeProvider>
-      <ShopProvider>{children}</ShopProvider>
+      <ShopProvider>
+        <div className="container">{children}</div>
+      </ShopProvider>
     </AppBridgeProvider>
   );
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  
   return (
     <html lang="en">
       <head>
